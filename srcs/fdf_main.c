be4884a218d95338a685492455cf3d5d4f341511
Arnalove <chansarelarno@gmail.com>
2023-01-20 20:20:14 +0100

    iafter eval2

--- srcs/fdf_main.c
+++ srcs/fdf_main.c
@@ -6,7 +6,7 @@
 /*   By: achansar <achansar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 16:34:01 by achansar          #+#    #+#             */
-/*   Updated: 2023/01/20 19:47:09 by achansar         ###   ########.fr       */
+/*   Updated: 2023/01/20 20:20:00 by achansar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int	destroy(t_data *set)
 	free_matrix(set->matrix.mtx, set->matrix.h - 1);
 	mlx_destroy_image(set->mlx, set->img.img);
 	mlx_destroy_window(set->mlx, set->win);
-	free(set->mlx);
 	exit(0);
 }
 
@@ -32,6 +31,11 @@ static int	open_window(t_data *set)
 	if (!set->win)
 		return (1);
 	set->img.img = mlx_new_image(set->mlx, WIDTH, HEIGHT);
+	if (!set->img.img)
+	{
+		mlx_destroy_window(set->mlx, set->win);
+		return (1);
+	}
 	set->img.addr = mlx_get_data_addr(set->img.img, &set->img.bpp,
 			&set->img.szline, &set->img.endian);
 	return (0);
@@ -50,7 +54,10 @@ int	main(int argc, char *argv[])
 			return (ft_error_msg("ERROR : Matrix couldn't be created."));
 		isometric(&set.matrix);
 		if (open_window(&set) != 0)
+		{
+			free_matrix(set.matrix.mtx, set.matrix.h - 1);
 			return (ft_error_msg("ERROR : Failed to open window."));
+		}
 		drawline_all(&set, &set.img, &set.matrix, set.matrix.mtx);
 		mlx_hook(set.win, KEYPRESS, (1L << 0), &ft_keys, &set);
 		mlx_hook(set.win, 17, 0, &destroy, &set);
